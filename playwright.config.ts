e7caab198cedeb09918cc5b392eb24a3efe21ee0
bilenh <bilen.haile@gmail.com>
2025-06-19 12:42:31 +0100

    "Updated config to run test on multiple broswers"

--- playwright.config.ts
+++ playwright.config.ts
@@ -3,11 +3,25 @@ import { defineConfig } from '@playwright/test';
 export default defineConfig({
   testDir: './tests',
   use: {
-    headless: false,
+    headless: true,
     baseURL: 'https://www.booking.com/flights',
     trace: 'on-first-retry',
     screenshot: 'only-on-failure',
     video: 'retain-on-failure',
   },
-  reporter: [['list'],["allure-playwright"]],
+  reporter: [['list'], ["allure-playwright"]],
+  projects: [
+    {
+      name: 'Chromium',
+      use: { browserName: 'chromium' },
+    },
+    {
+      name: 'Firefox',
+      use: { browserName: 'firefox' },
+    },
+    {
+      name: 'WebKit',
+      use: { browserName: 'webkit' },
+    },
+  ],
 });
